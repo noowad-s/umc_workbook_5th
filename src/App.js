c0ba96d979466465d5040ec52c87384ec06a8b9d
noowad-s <sdw0917@naver.com>
2023-12-02 01:22:00 +0900

    deploy

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-// function App() {
-//   return (
-//     <div className="App">
-//     </div>
-    
-//   );
-// }
-
-// export default App;
-
 import React from 'react';
 import Header from './Components/Header'; // Header 컴포넌트 import
-// import Movie from './Components/Movie'; 
 import TV from './pages/TV'; 
 import Celebrity from './pages/Celebrity'; 
 import Movies from './pages/Movies'; 
 import Home from './pages/Home'; 
 import NotFound from './pages/NotFound';
 import Login from './pages/Login';
+// import Login2 from './pages/Login2'
 import MovieDetail from './pages/MovieDetail';
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 function App() {
@@ -31,19 +21,11 @@ function App() {
           <Route path="/TV" element={<TV/>} />
           <Route path="/Celebrity" element={<Celebrity/>} />
           <Route path="/Login" element={<Login/>} />
+          {/* <Route path="/Login2" element={<Login2/>} /> */}
           <Route path="/*" element={<NotFound/>} />
           <Route path="/movie/:title" element={<MovieDetail/>} />
         </Routes>
       </BrowserRouter>
-      {/* {movies.results.map((movie, index) => (
-        <Movie
-          key={index}
-          title={movie.title}
-          posterUrl={movie.poster_path}
-          overview={movie.overview}
-          voteAverage={movie.vote_average}
-        />
-      ))} */}
     </div>
   );
 }
